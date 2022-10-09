006b2734337aad66719274beb069ad260e309610
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-09 15:52:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-css-porfolio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hqwmmw)
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+import { ThemeProvider } from './context';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
