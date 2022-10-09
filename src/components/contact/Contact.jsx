222a3f608e22b6f7b6ce61225ee8bf63a71ca564
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-09 23:24:19 +0700

    1 file modified

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,15 +1,12 @@
-import React from "react"
-import "./contact.css";
-// import Phone from "../../img/phone.png";
-// import Email from "../../img/email.png";
-// import Address from "../../img/address.png";
-import { useContext, useRef, useState } from "react";
-import emailjs from "emailjs-com";
-import { ThemeContext } from "../../context";
+import React from 'react';
+import './contact.css';
+import { useContext, useRef, useState } from 'react';
+import emailjs from 'emailjs-com';
+import { ThemeContext } from '../../context';
 
 const Contact = () => {
   const formRef = useRef();
-  const [done, setDone] = useState(false)
+  const [done, setDone] = useState(false);
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
@@ -17,15 +14,15 @@ const Contact = () => {
     e.preventDefault();
     emailjs
       .sendForm(
-        "service_c2edgo4",
-        "template_5k2dvib",
+        'service_c2edgo4',
+        'template_5k2dvib',
         formRef.current,
-        "BET_7-tzu8R4Fzrxk"
+        'BET_7-tzu8R4Fzrxk'
       )
       .then(
         (result) => {
           console.log(result.text);
-          setDone(true)
+          setDone(true);
         },
         (error) => {
           console.log(error.text);
@@ -41,16 +38,28 @@ const Contact = () => {
           <h1 className="c-title">Let's discuss your project</h1>
           <div className="c-info">
             <div className="c-info-item">
-              <img src="" alt="" className="c-icon" />
-              +1 1234 556 75
+              <img
+                src="https://res.cloudinary.com/daxrdiiyr/image/upload/v1665311919/dungthinh/phone_ltvhuc.png"
+                alt=""
+                className="c-icon"
+              />
+              +84 888 244 133
             </div>
             <div className="c-info-item">
-              <img className="c-icon" src="" alt="" />
-              contact@lama.dev
+              <img
+                className="c-icon"
+                src="https://res.cloudinary.com/daxrdiiyr/image/upload/v1665311919/dungthinh/email_trhizp.png"
+                alt=""
+              />
+              dungthinhtv@gmail.com
             </div>
             <div className="c-info-item">
-              <img className="c-icon" src="" alt="" />
-              245 King Street, Touterie Victoria 8520 Australia
+              <img
+                className="c-icon"
+                src="https://res.cloudinary.com/daxrdiiyr/image/upload/v1665311919/dungthinh/address_coxq4d.png"
+                alt=""
+              />
+              588 Nguyen Thi Dinh, Thu Duc, HCMC, Vietnam
             </div>
           </div>
         </div>
@@ -60,12 +69,32 @@ const Contact = () => {
             freelancing if the right project comes along. me.
           </p>
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
-            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
-            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
-            <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message" />
+            <input
+              style={{ backgroundColor: darkMode && '#333' }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && '#333' }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && '#333' }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && '#333' }}
+              rows="5"
+              placeholder="Message"
+              name="message"
+            />
             <button>Submit</button>
-            {done && "Thank you..."}
+            {done && 'Thank you...'}
           </form>
         </div>
       </div>
