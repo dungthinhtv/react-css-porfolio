8df73ed00685687d88d222f4488cbe59cb3fce1a
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-09 17:46:08 +0700

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from './context';
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
